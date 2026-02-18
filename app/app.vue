8054bda62811221fd956fec8e25fbbdba251b685
MarcMercury <marc.h.mercury@gmail.com>
2026-02-18 08:10:39 +0000

    fix: force component re-render on route change
    - Add :key attribute to NuxtPage based on route.fullPath
    - Fixes issue where URL changes but page doesn't visually update
    - Forces Vue to unmount/remount components on navigation

--- app/app.vue
+++ app/app.vue
@@ -4,10 +4,11 @@
  * Data hydration happens in the layout, not here
  * This keeps the app.vue simple
  */
+const route = useRoute()
 </script>
 
 <template>
   <NuxtLayout>
-    <NuxtPage />
+    <NuxtPage :key="route.fullPath" />
   </NuxtLayout>
 </template>
